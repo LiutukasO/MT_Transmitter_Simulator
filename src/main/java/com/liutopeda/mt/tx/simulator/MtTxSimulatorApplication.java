0aeb4643fee725f5172e704428f3fea9c798a60b
Ovidijus Pukys <liutukaso@gmail.com>
2022-07-24 18:36:01 +0300

    Java src init

---
+++ src/main/java/com/liutopeda/mt/tx/simulator/MtTxSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.liutopeda.mt.tx.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtTxSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtTxSimulatorApplication.class, args);
+    }
+
+}
